7cf566f09bffac607070d31ed502b6f3e664c9b1
Tauseef Ahmad <120765267+TauseefAlpha@users.noreply.github.com>
2026-04-02 20:11:14 +0500

    chore: temporarily skip unit tests in CI to prioritize distribution builds

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-reanimated|react-native-worklets|react-native-worklets-core|react-native-svg|lucide-react-native)/)',
+  ],
 };
---
+++ jest.setup.js
@@ -0,0 +1,40 @@
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+// Comprehensive mock for Worklets (needed for Reanimated 4)
+const ID = (v) => v;
+const NOOP = () => {};
+const mockWorklets = {
+  __esModule: true,
+  createSerializable: ID,
+  isWorkletFunction: () => false,
+  isShareableRef: () => true,
+  makeShareable: ID,
+  makeShareableCloneOnUIRecursive: ID,
+  makeShareableCloneRecursive: ID,
+  shareableMappingCache: new Map(),
+  serializableMappingCache: new Map(),
+  getStaticFeatureFlag: () => false,
+  setDynamicFeatureFlag: NOOP,
+  isSynchronizable: () => false,
+  getRuntimeKind: () => 1, // RuntimeKind.ReactNative
+  RuntimeKind: { ReactNative: 1, UI: 2, Worklet: 3 },
+  createWorkletRuntime: () => ({}),
+  runOnRuntime: ID,
+  scheduleOnRuntime: (cb) => cb(),
+  runOnJS: (v) => v,
+  runOnUI: (v) => v,
+  runOnUIAsync: (v) => Promise.resolve(),
+  makeMutable: (v) => ({ value: v }),
+  scheduleOnUI: (v) => v,
+};
+
+jest.mock('react-native-worklets', () => mockWorklets);
+jest.mock('react-native-worklets-core', () => mockWorklets);
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.useReducedMotion = () => false;
+  return Reanimated;
+});
